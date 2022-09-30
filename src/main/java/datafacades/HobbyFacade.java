13c41e206fc81f12e8f7d0e149f271e5503ff689
legolai <nico360f@gmail.com>
2022-09-30 14:47:10 +0200

    Add HobbyFacade

---
+++ src/main/java/datafacades/HobbyFacade.java
@@ -0,0 +1,85 @@
+package datafacades;
+
+import entities.Hobby;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class HobbyFacade implements IDataFacade<Hobby> {
+
+    private static HobbyFacade instance;
+    private static EntityManagerFactory emf;
+
+    //Private Constructor to ensure Singleton
+    private HobbyFacade() {}
+
+
+    /**
+     *
+     * @param _emf
+     * @return an instance of this facade class.
+     */
+    public static HobbyFacade getInstance(EntityManagerFactory _emf) {
+
+        if (instance == null) {
+            emf = _emf;
+            instance = new HobbyFacade();
+        }
+        return instance;
+    }
+
+    @Override
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public Hobby create(String name, String description, String category, String type) {
+        Hobby hobby = new Hobby();
+        hobby.setName(name);
+        hobby.setDescription(description);
+        hobby.setCategory(category);
+        hobby.setType(type);
+        return create(hobby);
+    }
+
+    @Override
+    public Hobby create(Hobby hobby) {
+        executeInsideTransaction(em -> em.persist(hobby));
+        return hobby;
+    }
+
+    @Override
+    public <String> Hobby getById(String name) {
+        Hobby hobby = executeWithClose(em -> em.find(Hobby.class, name));
+        if (hobby == null)
+            throw new EntityNotFoundException("Hobby called " + name + " does not exists!");
+        return hobby;
+    }
+
+    @Override
+    public List<Hobby> getAll() {
+        return executeWithClose(em -> {
+            TypedQuery<Hobby> query = em.createQuery("SELECT h FROM Hobby h", Hobby.class);
+            return query.getResultList();
+        });
+    }
+
+    @Override
+    public Hobby update(Hobby hobby) {
+        if (getById(hobby.getName()) == null) throw new EntityNotFoundException();
+        executeInsideTransaction(em -> em.merge(hobby));
+        return hobby;
+    }
+
+    @Override
+    public <String> void delete(String name) {
+        executeInsideTransaction(em -> {
+            Hobby hobby = em.find(Hobby.class, name);
+            if (hobby == null) throw new EntityNotFoundException("Hobby called " + name + " does not exists!");
+            em.remove(hobby);
+        });
+    }
+}
--- src/main/java/entities/Hobby.java
+++ src/main/java/entities/Hobby.java
@@ -6,9 +6,9 @@ import java.util.Objects;
 import java.util.Set;
 
 @Entity
+@NamedQuery(name = "Hobby.deleteAllRows", query = "DELETE from Hobby")
 public class Hobby {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "name", nullable = false, length = 45)
     private String name;
     @Column(name = "description", nullable = false, length = 45)
@@ -24,8 +24,8 @@ public class Hobby {
     public Hobby() {
 
     }
-    public Hobby(String id, String description, String category, String type, Set<Person> people) {
-        this.name = id;
+    public Hobby(String name, String description, String category, String type, Set<Person> people) {
+        this.name = name;
         this.description = description;
         this.category = category;
         this.type = type;
@@ -35,8 +35,8 @@ public class Hobby {
     public String getName() {
         return name;
     }
-    public void setName(String id) {
-        this.name = id;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getDescription() {
@@ -69,7 +69,7 @@ public class Hobby {
 
     @Override
     public String toString() {
-        return "Hobby{" + "id='" + name + '\'' + ", description='" + description + '\'' + ", category='" + category + '\'' + ", type='" + type + '\'' + ", people=" + people + '}';
+        return "Hobby{" + "name='" + name + '\'' + ", description='" + description + '\'' + ", category='" + category + '\'' + ", type='" + type + '\'' + ", people=" + people + '}';
     }
     @Override
     public boolean equals(Object o) {
