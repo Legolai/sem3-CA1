55764a391de3017d8f9eb5b43d1eb46dc5f0beab
legolai <nico360f@gmail.com>
2022-10-03 15:21:37 +0200

    Add CityInfoDTOFacade

---
+++ src/main/java/businessfacades/CityInfoDTOFacade.java
@@ -0,0 +1,49 @@
+package businessfacades;
+
+import datafacades.CityInfoFacade;
+import datafacades.HobbyFacade;
+import dtos.CityInfoDTO;
+
+import javax.persistence.EntityManagerFactory;
+import java.util.List;
+
+public class CityInfoDTOFacade implements IDataDTOFacade<CityInfoDTO, String> {
+    private static CityInfoDTOFacade instance;
+    private static CityInfoFacade cityInfoFacade;
+
+    private CityInfoDTOFacade() {}
+
+    public static CityInfoDTOFacade getInstance(EntityManagerFactory _emf) {
+        if (instance == null) {
+            cityInfoFacade = CityInfoFacade.getInstance(_emf);
+            instance = new CityInfoDTOFacade();
+        }
+        return instance;
+    }
+
+    @Override
+    public CityInfoDTO create(CityInfoDTO cityInfoDTO) {
+        return new CityInfoDTO(cityInfoFacade.create(cityInfoDTO.getEntity()));
+    }
+
+    @Override
+    public CityInfoDTO getById(String zipCode) {
+        return new CityInfoDTO(cityInfoFacade.getById(zipCode));
+    }
+
+    @Override
+    public List<CityInfoDTO> getAll() {
+        return CityInfoDTO.toList(cityInfoFacade.getAll());
+    }
+
+    @Override
+    public CityInfoDTO update(CityInfoDTO cityInfoDTO) {
+        return new CityInfoDTO(cityInfoFacade.update(cityInfoDTO.getEntity()));
+    }
+
+    @Override
+    public void delete(String zipCode) {
+        cityInfoFacade.delete(zipCode);
+    }
+
+}
